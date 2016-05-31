0456d8c1942d8462642382e89574ee11630e8caa
Marc-Aurèle Brothier <m@brothier.org>
2016-05-31 14:00:24 +0200

    fix app to load on tomcat

--- src/main/java/com/peakxl/sample/Application.java
+++ src/main/java/com/peakxl/sample/Application.java
@@ -1,24 +1,20 @@
 package com.peakxl.sample;
 
-import java.util.Arrays;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Application {
+public class Application extends SpringBootServletInitializer {
 
-    public static void main(String[] args) {
-        ApplicationContext ctx = SpringApplication.run(Application.class, args);
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(Application.class);
+    }
 
-        System.out.println("Let's inspect the beans provided by Spring Boot:");
-
-        String[] beanNames = ctx.getBeanDefinitionNames();
-        Arrays.sort(beanNames);
-        for (String beanName : beanNames) {
-            System.out.println(beanName);
-        }
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(Application.class, args);
     }
 
 }
